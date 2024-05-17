af1496ce45d327369ac1ade7a557363c721c631d
Nikolin Majmari <nikolinmajmari30@gmail.com>
2024-05-17 19:33:38 +0200

    gallery part 2, added layout component

--- custom-modules/awesome_gallery/static/src/gallery_controller/gallery_controller.js
+++ custom-modules/awesome_gallery/static/src/gallery_controller/gallery_controller.js
@@ -1,10 +1,12 @@
 /** @odoo-module **/
 import { Component } from "@odoo/owl";
 import { standardViewProps } from "@web/views/standard_view_props";
+import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.controller";
     static components = {
+        Layout
     };
     static props = {
         ...standardViewProps

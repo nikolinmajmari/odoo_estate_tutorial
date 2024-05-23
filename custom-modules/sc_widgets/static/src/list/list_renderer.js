a9c0de5a8bec7e398f3e3292076a59a36899baf6
Nikolin Majmari <nikolinmajmari30@gmail.com>
2024-05-23 17:52:43 +0200

    removed all js code

--- custom-modules/sc_widgets/static/src/list/list_renderer.js
+++ custom-modules/sc_widgets/static/src/list/list_renderer.js
@@ -1,19 +1,19 @@
-/** @odoo-module **/
+// /** @odoo-module **/
 
 
-import {ListRenderer} from "@web/views/list/list_renderer";
+// import {ListRenderer} from "@web/views/list/list_renderer";
 
 
-export class SCListRenderer extends ListRenderer{
-    // static template = "sc_widgets.ListRenderer";
-    static props = {
-        ...ListRenderer.props
-    };
-    static components = {
-        ...ListRenderer.components
-    };
+// export class SCListRenderer extends ListRenderer{
+//     // static template = "sc_widgets.ListRenderer";
+//     static props = {
+//         ...ListRenderer.props
+//     };
+//     static components = {
+//         ...ListRenderer.components
+//     };
 
-    setup(){
-        super.setup();
-    }
-}
+//     setup(){
+//         super.setup();
+//     }
+// }
